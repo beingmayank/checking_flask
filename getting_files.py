0cc3b728a323aff4fda1a28d56653829571be46e
beingmayank <mayankbhardwaj0085@gmail.com>
2024-11-14 21:32:17 +0530

    first code

---
+++ getting_files.py
@@ -0,0 +1,12 @@
+'''
+1. install git: https://git-scm.com/downloads/win
+
+2. check on terminal: 'git --version' for succesfull installation
+
+3. go to repository and click on code:
+    copy the link
+    and past on terminal
+
+    eg. git clone {link}
+    
+'''
---
+++ uploading_files.py
@@ -0,0 +1,12 @@
+'''
+create new repository and enter these command one by one:
+
+1. git init
+2. git add README.md
+3. git commit -m "first commit"
+4. git branch -M main
+5. git remote add origin https://github.com/beingmayank/checking_flask.git
+6. git push -u origin main
+
+
+'''
